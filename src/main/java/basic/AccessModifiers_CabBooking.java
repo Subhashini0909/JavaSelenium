d83065c337c2f7af2e55b10dbf05fc5b84bbddc8
Subhashini0909 <c.subhashini09@gmail.com>
2024-03-05 14:54:41 +0530

    Access Modifiers

---
+++ src/main/java/basic/AccessModifiers_CabBooking.java
@@ -0,0 +1,37 @@
+package basic;
+
+public class AccessModifiers_CabBooking {
+
+	public static void main(String[] args) 
+	{
+		AccessModifiers_CabBooking obj = new AccessModifiers_CabBooking();
+		obj.Cab_Seat_Availability_Checking();
+		String User = obj.User_Login();
+		System.out.println("Enter User detail : " + User);
+		boolean CAC = obj.Cab_Availability_Checking();
+		System.out.println("Cabs are available? " +CAC);
+
+	}
+	
+	//PUBLIC
+	public byte Cab_Seat_Availability_Checking() //Can Be used anywhere
+	{
+		byte Cab_Seat_Availability = 99;
+		System.out.println("No.of seats available : " +Cab_Seat_Availability);
+		return Cab_Seat_Availability;
+	}
+	
+	//DEFAULT
+	boolean Cab_Availability_Checking() //Can be used anywhere inside the package
+	{
+		return true;
+	}
+	
+	
+	//PRIVATE
+	private String User_Login() //Can be used only within the class
+	{
+		return "Subha";
+	}
+	
+}

eb63126e814540cfbbd9b2ddcd85fed9bb22fa68
Subhashini0909 <c.subhashini09@gmail.com>
2024-03-08 21:20:13 +0530

    Constructor

---
+++ src/main/java/basic/ConstructorChaining.java
@@ -0,0 +1,30 @@
+package basic;
+
+public class ConstructorChaining {
+
+	public static void main(String[] args) 
+	{
+		ConstructorChaining cc = new ConstructorChaining(); //1
+			//ConstructorChaining cc1 = new ConstructorChaining("hello"); //2
+
+	}
+	
+	ConstructorChaining()
+	{
+		this("Hi"); // 1(a) This calls parameterized constructor with string
+		System.out.println("Default Constructor"); //4 And then Default Constructor will be called
+	}
+	
+	ConstructorChaining(String a)
+	{
+		this(1); //2 //This calls parameterized constructor with int 1st
+		System.out.println("Parameterized Constructor with String datatype"); //3
+	}
+	
+	ConstructorChaining(int a) //2
+	{
+		System.out.println("Parameterized Constructor with int datatypes");
+	}
+	
+
+}

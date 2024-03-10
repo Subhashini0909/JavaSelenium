23e9618718ed80da53cdf7a7f565e6cb0c25f80b
Subhashini0909 <c.subhashini09@gmail.com>
2024-03-10 12:38:43 +0530

    Navigation commands

---
+++ src/main/java/selenium/Navigation.java
@@ -0,0 +1,33 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Navigation {
+
+	public static void main(String[] args) 
+	{
+		ChromeDriver driver = new ChromeDriver();
+		
+//Get
+		driver.get("https://letcode.in/");
+		
+//Navigate To
+		driver.navigate().to("https://letcode.in/buttons");
+		
+	
+//Navigate Back
+		driver.navigate().back();
+		
+		String Navigated_URL = driver.getCurrentUrl();
+		System.out.println("Navigated URL : " + Navigated_URL);
+		
+//Forward
+		driver.navigate().forward();
+		
+//Refresh
+		driver.navigate().refresh();
+		driver.quit();
+	}
+
+}
